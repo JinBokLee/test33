4571e7b6de17403d62eb236841191587c4ce3aae
han3189 <han3189@nate.com>
2020-02-19 15:29:29 +0900

    222

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  print("aaa")
-  return 'Hello World56568886786885f222f6!'
+  print("aaa222222")
+  return 'Hello World56568886786885f222fsdfsdfsdf6!'
 
   # return render_template("main/index.html")
 
